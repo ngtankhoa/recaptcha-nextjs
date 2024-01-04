af49aa8e667f9496af7722bc72ae0bcefd78ae21
John Nguyen <john.nguyen@legato.co>
2024-01-04 13:37:49 +0700

    recaptcha V2 checkbox

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <script src='https://www.google.com/recaptcha/api.js' async defer></script>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>abc</div>
+      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+        <form action='?' method='POST'>
+          <div className='g-recaptcha' data-sitekey='6LfuTEUpAAAAADWOEefHUqLjEgkUyEMJ-9F6HayK'></div>
+          <br />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
     </main>
   )
 }
+
+// 6LfuTEUpAAAAADWOEefHUqLjEgkUyEMJ-9F6HayK
